efc16d39edcd183f86278e36c1e70f4bfa5b11a8
Joshua Anderson <joshua.anderson688@gmail.com>
2016-04-16 23:16:09 +0100

    Added sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var cleancss = require('gulp-clean-css');
 
-gulp.task('lintify', function () {
+gulp.task('lintify:src', function () {
 	return gulp
 		.src('src/**/*.{js,jsx}')
 		.pipe(eslint())
 		.pipe(eslint.format())
 		.pipe(eslint.failOnError());
+});
+
+gulp.task('build:styles', function () {
+	return gulp
+		.src('src/styles/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cleancss())
+		.pipe(sourcemaps.write('./maps'))
+		.pipe(gulp.dest('./public/styles'));
 });
 
 gulp.task('default', function() {
