c00261a57a7fbeb8d35c75430fd8eea8db03255b
Joshua Anderson <joshua.anderson688@gmail.com>
2016-04-16 22:36:00 +0100

    Added base outline for projecT

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var eslint = require('gulp-eslint');
+
+gulp.task('lintify', function () {
+	return gulp
+		.src('src/**/*.{js,jsx}')
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failOnError());
+});
 
 gulp.task('default', function() {
-	
+
 });
